e3236e98b4c61b6a4e97d58f113f775ce87c100b
Operation <695915099@qq.com>
2016-08-03 19:30:27 +0800

    Update result.sh

--- aliyun-cdn/result.sh
+++ aliyun-cdn/result.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #set -x
-#url=`python cdn.py Action=DescribeCdnDomainLogs DomainName=cdn.wecash.net LogDay=2016-01-12`
+#url=`python cdn.py Action=DescribeCdnDomainLogs DomainName=lx.wxqrcode.com LogDay=2016-01-12`
 url=`/usr/bin/python /data/scripts/cdn/cdn.py Action=RefreshObjectCaches ObjectType=Directory ObjectPath=http://lx.wxqrcode.com/`
 /usr/bin/curl $url
